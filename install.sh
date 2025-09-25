1393d4752e7bea9f154bcdca8f572f5e47f3b187
0xRh4ps00dy <marcosjurado83@gmail.com>
2025-09-25 09:55:20 +0200

    Replace '~' with '$HOME' in install.sh

--- install.sh
+++ install.sh
@@ -1,135 +1,135 @@
 sudo apt install -y bspwm sxhkd alacritty
 sleep 1
 
-mkdir -p ~/.config/bspwm
+mkdir -p $HOME/.config/bspwm
 sleep 1
 
-mkdir -p ~/.config/sxhkd
+mkdir -p $HOME/.config/sxhkd
 sleep 1
 
-cp -r ~/autoBSPWM/config/bspwm ~/.config
+cp -r $HOME/autoBSPWM/config/bspwm $HOME/.config
 sleep 1
 
-cp -r ~/autoBSPWM/config/sxhkd ~/.config
+cp -r $HOME/autoBSPWM/config/sxhkd $HOME/.config
 sleep 1
 
-chmod +x ~/.config/bspwm/bspwmrc
+chmod +x $HOME/.config/bspwm/bspwmrc
 sleep 1
 
 sudo apt install -y polybar rofi xcompmgr flameshot feh lsd numlockx
 sleep 1
 
 
-mkdir -p ~/.config/polybar
+mkdir -p $HOME/.config/polybar
 sleep 1
 
-mkdir -p ~/.config/rofi
+mkdir -p $HOME/.config/rofi
 sleep 1
 
-mkdir -p ~/.config/alacritty
+mkdir -p $HOME/.config/alacritty
 sleep 1
 
-# mkdir -p ~/.config/picom
+# mkdir -p $HOME/.config/picom
 # sleep 1
-cp -r ~/autoBSPWM/config/polybar ~/.config
+cp -r $HOME/autoBSPWM/config/polybar $HOME/.config
 sleep 1
 
-cp -r ~/autoBSPWM/config/rofi ~/.config
+cp -r $HOME/autoBSPWM/config/rofi $HOME/.config
 sleep 1
 
-cp -r ~/autoBSPWM/config/alacritty ~/.config
+cp -r $HOME/autoBSPWM/config/alacritty $HOME/.config
 sleep 1
 
-# cp -r ~/autoBSPWM/config/picom ~/.config
+# cp -r $HOME/autoBSPWM/config/picom $HOME/.config
 # sleep 1
 
-chmod +x ~/autoBSPWM/scripts/*
+chmod +x $HOME/autoBSPWM/scripts/*
 sleep 1
 
-chmod +x ~/.config/polybar/launch.sh
+chmod +x $HOME/.config/polybar/launch.sh
 sleep 1
 
-chmod +x ~/.config/polybar/forest/scripts/*
+chmod +x $HOME/.config/polybar/forest/scripts/*
 sleep 1
 
-chmod +x ~/.config/polybar/forest/launch.sh
+chmod +x $HOME/.config/polybar/forest/launch.sh
 sleep 1
 
-mkdir -p ~/Wallpapers
+mkdir -p $HOME/Wallpapers
 sleep 1
 
-cp ~/autoBSPWM/wallpapers/kali.png ~/Wallpapers
+cp $HOME/autoBSPWM/wallpapers/kali.png $HOME/Wallpapers
 sleep 1
 
-cp ~/autoBSPWM/scripts/ethernet_status.sh ~/.config/polybar/forest/scripts/
+cp $HOME/autoBSPWM/scripts/ethernet_status.sh $HOME/.config/polybar/forest/scripts/
 sleep 1
 
-cp ~/autoBSPWM/scripts/vpn_status.sh ~/.config/polybar/forest/scripts/
+cp $HOME/autoBSPWM/scripts/vpn_status.sh $HOME/.config/polybar/forest/scripts/
 sleep 1
 
-cp ~/autoBSPWM/scripts/htb_target.sh  ~/.config/polybar/forest/scripts/
+cp $HOME/autoBSPWM/scripts/htb_target.sh  $HOME/.config/polybar/forest/scripts/
 sleep 1
 
-touch ~/.target
+touch $HOME/.target
 sleep 1
 
-cp -r ~/autoBSPWM/fonts ~/.local/share/fonts
+cp -r $HOME/autoBSPWM/fonts $HOME/.local/share/fonts
 sleep 1
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sleep 1
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sleep 1
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sleep 1
 
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-sleep 1
-
-
-cp ~/autoBSPWM/.zshrc ~/.zshrc
-sleep 1
-
-cp ~/autoBSPWM/functions ~/.config/functions
-sleep 1
-
-cp ~/autoBSPWM/aliases ~/.config/aliases
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 sleep 1
 
 
-
-sudo ln -s ~/autoBSPWM/scripts/ligolo_script.sh /usr/local/bin/ligolo_script
+cp $HOME/autoBSPWM/.zshrc $HOME/.zshrc
 sleep 1
 
-sudo ln -s ~/autoBSPWM/scripts/multihandler.sh /usr/local/bin/multihandler
+cp $HOME/autoBSPWM/functions $HOME/.config/functions
 sleep 1
 
-sudo ln -s ~/autoBSPWM/scripts/penelope.py /usr/local/bin/pen
+cp $HOME/autoBSPWM/aliases $HOME/.config/aliases
 sleep 1
 
-sudo ln -s ~/autoBSPWM/scripts/target.sh /usr/local/bin/target
+
+
+sudo ln -s $HOME/autoBSPWM/scripts/ligolo_script.sh /usr/local/bin/ligolo_script
 sleep 1
 
-sudo ln -s ~/autoBSPWM/scripts/transfile.sh /usr/local/bin/transfile
+sudo ln -s $HOME/autoBSPWM/scripts/multihandler.sh /usr/local/bin/multihandler
+sleep 1
+
+sudo ln -s $HOME/autoBSPWM/scripts/penelope.py /usr/local/bin/pen
+sleep 1
+
+sudo ln -s $HOME/autoBSPWM/scripts/target.sh /usr/local/bin/target
+sleep 1
+
+sudo ln -s $HOME/autoBSPWM/scripts/transfile.sh /usr/local/bin/transfile
 sleep 1
 
 
 alacritty migrate
 sleep 1
 
-touch ~/.ctf_vars
+touch $HOME/.ctf_vars
 sleep 1
 
 sudo apt install -y ufw sshpass bat
 sleep 1
 
-sudo cp ~/autoBSPWM/scripts/proxy /usr/local/bin/proxy
+sudo cp $HOME/autoBSPWM/scripts/proxy /usr/local/bin/proxy
 sleep 1
 
 sudo chmod +x /usr/local/bin/proxy
 sleep 1
 
-sudo chmod +x /home/rh4ps00dy/.config/bspwm/scripts/bspwm_resize
+sudo chmod +x $HOME/.config/bspwm/scripts/bspwm_resize
 sleep 1
